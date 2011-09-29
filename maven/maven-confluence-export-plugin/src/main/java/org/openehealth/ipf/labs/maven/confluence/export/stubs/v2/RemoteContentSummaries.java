4219ee590870df31e14b7b8467998f4f652660e3
stanojevic-boris <stanojevic.boris@gmail.com>
2011-09-29 14:33:45 +0200

    added generated stubs for confluence API v2

---
+++ maven/maven-confluence-export-plugin/src/main/java/org/openehealth/ipf/labs/maven/confluence/export/stubs/v2/RemoteContentSummaries.java
@@ -0,0 +1,191 @@
+/**
+ * RemoteContentSummaries.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package org.openehealth.ipf.labs.maven.confluence.export.stubs.v2;
+
+public class RemoteContentSummaries  implements java.io.Serializable {
+    private org.openehealth.ipf.labs.maven.confluence.export.stubs.RemoteContentSummary[] content;
+
+    private int offset;
+
+    private int totalAvailable;
+
+    public RemoteContentSummaries() {
+    }
+
+    public RemoteContentSummaries(
+           org.openehealth.ipf.labs.maven.confluence.export.stubs.RemoteContentSummary[] content,
+           int offset,
+           int totalAvailable) {
+           this.content = content;
+           this.offset = offset;
+           this.totalAvailable = totalAvailable;
+    }
+
+
+    /**
+     * Gets the content value for this RemoteContentSummaries.
+     * 
+     * @return content
+     */
+    public org.openehealth.ipf.labs.maven.confluence.export.stubs.RemoteContentSummary[] getContent() {
+        return content;
+    }
+
+
+    /**
+     * Sets the content value for this RemoteContentSummaries.
+     * 
+     * @param content
+     */
+    public void setContent(org.openehealth.ipf.labs.maven.confluence.export.stubs.RemoteContentSummary[] content) {
+        this.content = content;
+    }
+
+
+    /**
+     * Gets the offset value for this RemoteContentSummaries.
+     * 
+     * @return offset
+     */
+    public int getOffset() {
+        return offset;
+    }
+
+
+    /**
+     * Sets the offset value for this RemoteContentSummaries.
+     * 
+     * @param offset
+     */
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+
+    /**
+     * Gets the totalAvailable value for this RemoteContentSummaries.
+     * 
+     * @return totalAvailable
+     */
+    public int getTotalAvailable() {
+        return totalAvailable;
+    }
+
+
+    /**
+     * Sets the totalAvailable value for this RemoteContentSummaries.
+     * 
+     * @param totalAvailable
+     */
+    public void setTotalAvailable(int totalAvailable) {
+        this.totalAvailable = totalAvailable;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof RemoteContentSummaries)) return false;
+        RemoteContentSummaries other = (RemoteContentSummaries) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.content==null && other.getContent()==null) || 
+             (this.content!=null &&
+              java.util.Arrays.equals(this.content, other.getContent()))) &&
+            this.offset == other.getOffset() &&
+            this.totalAvailable == other.getTotalAvailable();
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getContent() != null) {
+            for (int i=0;
+                 i<java.lang.reflect.Array.getLength(getContent());
+                 i++) {
+                java.lang.Object obj = java.lang.reflect.Array.get(getContent(), i);
+                if (obj != null &&
+                    !obj.getClass().isArray()) {
+                    _hashCode += obj.hashCode();
+                }
+            }
+        }
+        _hashCode += getOffset();
+        _hashCode += getTotalAvailable();
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(RemoteContentSummaries.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://beans.soap.rpc.confluence.atlassian.com", "RemoteContentSummaries"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("content");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "content"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://beans.soap.rpc.confluence.atlassian.com", "RemoteContentSummary"));
+        elemField.setNillable(true);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("offset");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "offset"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("totalAvailable");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "totalAvailable"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
---
+++ maven/maven-confluence-export-plugin/src/main/java/org/openehealth/ipf/labs/maven/confluence/export/stubs/v2/RemoteSearchResult.java
@@ -0,0 +1,259 @@
+/**
+ * RemoteSearchResult.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package org.openehealth.ipf.labs.maven.confluence.export.stubs.v2;
+
+public class RemoteSearchResult  implements java.io.Serializable {
+    private java.lang.String excerpt;
+
+    private long id;
+
+    private java.lang.String title;
+
+    private java.lang.String type;
+
+    private java.lang.String url;
+
+    public RemoteSearchResult() {
+    }
+
+    public RemoteSearchResult(
+           java.lang.String excerpt,
+           long id,
+           java.lang.String title,
+           java.lang.String type,
+           java.lang.String url) {
+           this.excerpt = excerpt;
+           this.id = id;
+           this.title = title;
+           this.type = type;
+           this.url = url;
+    }
+
+
+    /**
+     * Gets the excerpt value for this RemoteSearchResult.
+     * 
+     * @return excerpt
+     */
+    public java.lang.String getExcerpt() {
+        return excerpt;
+    }
+
+
+    /**
+     * Sets the excerpt value for this RemoteSearchResult.
+     * 
+     * @param excerpt
+     */
+    public void setExcerpt(java.lang.String excerpt) {
+        this.excerpt = excerpt;
+    }
+
+
+    /**
+     * Gets the id value for this RemoteSearchResult.
+     * 
+     * @return id
+     */
+    public long getId() {
+        return id;
+    }
+
+
+    /**
+     * Sets the id value for this RemoteSearchResult.
+     * 
+     * @param id
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    /**
+     * Gets the title value for this RemoteSearchResult.
+     * 
+     * @return title
+     */
+    public java.lang.String getTitle() {
+        return title;
+    }
+
+
+    /**
+     * Sets the title value for this RemoteSearchResult.
+     * 
+     * @param title
+     */
+    public void setTitle(java.lang.String title) {
+        this.title = title;
+    }
+
+
+    /**
+     * Gets the type value for this RemoteSearchResult.
+     * 
+     * @return type
+     */
+    public java.lang.String getType() {
+        return type;
+    }
+
+
+    /**
+     * Sets the type value for this RemoteSearchResult.
+     * 
+     * @param type
+     */
+    public void setType(java.lang.String type) {
+        this.type = type;
+    }
+
+
+    /**
+     * Gets the url value for this RemoteSearchResult.
+     * 
+     * @return url
+     */
+    public java.lang.String getUrl() {
+        return url;
+    }
+
+
+    /**
+     * Sets the url value for this RemoteSearchResult.
+     * 
+     * @param url
+     */
+    public void setUrl(java.lang.String url) {
+        this.url = url;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof RemoteSearchResult)) return false;
+        RemoteSearchResult other = (RemoteSearchResult) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.excerpt==null && other.getExcerpt()==null) || 
+             (this.excerpt!=null &&
+              this.excerpt.equals(other.getExcerpt()))) &&
+            this.id == other.getId() &&
+            ((this.title==null && other.getTitle()==null) || 
+             (this.title!=null &&
+              this.title.equals(other.getTitle()))) &&
+            ((this.type==null && other.getType()==null) || 
+             (this.type!=null &&
+              this.type.equals(other.getType()))) &&
+            ((this.url==null && other.getUrl()==null) || 
+             (this.url!=null &&
+              this.url.equals(other.getUrl())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getExcerpt() != null) {
+            _hashCode += getExcerpt().hashCode();
+        }
+        _hashCode += new Long(getId()).hashCode();
+        if (getTitle() != null) {
+            _hashCode += getTitle().hashCode();
+        }
+        if (getType() != null) {
+            _hashCode += getType().hashCode();
+        }
+        if (getUrl() != null) {
+            _hashCode += getUrl().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(RemoteSearchResult.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://beans.soap.rpc.confluence.atlassian.com", "RemoteSearchResult"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("excerpt");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "excerpt"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(true);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("id");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "id"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "long"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("title");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "title"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(true);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("type");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "type"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(true);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("url");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "url"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setNillable(true);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
